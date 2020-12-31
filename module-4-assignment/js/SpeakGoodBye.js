f456a3ac4a3b8dfcf558a29cee795cd3fbec58a0
nbaldzhiev <nenko.baldzhiev@gmail.com>
2020-12-31 19:32:45 +0200

    add module 4 assignment solution

---
+++ module-4-assignment/js/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(
+	function SpeakGoodBye (window) {
+		var speakWord = "Good Bye";
+		var byeSpeaker = {
+			speak: function (name) {
+				console.log(speakWord + " " + name);
+			}
+		};
+		window.byeSpeaker = byeSpeaker;
+	}
+)(window);
---
+++ module-4-assignment/js/SpeakHello.js
@@ -0,0 +1,11 @@
+(
+	function SpeakHello (window) {
+		var speakWord = "Hello";
+		var helloSpeaker = {
+			speak: function (name) {
+				console.log(speakWord + " " + name);
+			}
+		};
+		window.helloSpeaker = helloSpeaker;
+	}
+)(window);

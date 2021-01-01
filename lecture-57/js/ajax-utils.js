3f405c2c0afd0fcb4cda2dcc0071673a26398af0
nbaldzhiev <nenko.baldzhiev@gmail.com>
2021-01-01 23:57:25 +0200

    add lecture-57 files

---
+++ lecture-57/js/ajax-utils.js
@@ -0,0 +1,35 @@
+(function (global) {
+
+var ajaxUtils = {};
+
+function getRequestObject() {
+	if (global.XMLHttpRequest) {
+		return (new XMLHttpRequest());
+	}
+	else if (global.ActiveXObject) {
+		return (new ActiveXObject("Microsoft.XMLHTTP"));
+	}
+	else {
+		global.alert("Ajax is not supported");
+		return (null);
+	}
+}
+
+ajaxUtils.sendGetRequest = function(requestURL, responseHandler) {
+	var request = getRequestObject();
+	request.onreadystatechange = function() {
+		handleResponse(request, responseHandler);
+	};
+	request.open("GET", requestURL, true);
+	request.send(null);
+};
+
+function handleResponse(request, responseHandler) {
+	if ((request.readyState == 4) && (request.status == 200)) {
+		responseHandler(request);
+	}
+}
+
+global.$ajaxUtils = ajaxUtils;
+}
+)(window);

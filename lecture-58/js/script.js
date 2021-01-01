87aac5bf4ac127463b3cd569a95805b20743e9da
nbaldzhiev <nenko.baldzhiev@gmail.com>
2021-01-02 00:24:54 +0200

    add lecture-58 data

---
+++ lecture-58/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+		document.querySelector("button").addEventListener("click",
+			function () {
+				$ajaxUtils.sendGetRequest("/data/name.json", 
+					function (res) {
+
+						var message = res.firstName + " " + res.lastName;
+						if (res.likesChineseFood) {
+							message += " likes Chinese food";
+						}
+						else {
+							message += " does not like Chinese food";
+						}
+						message += " and uses ";
+						message += res.numberOfDisplays;
+						message += " displays for coding.";
+
+						document.querySelector("#content").innerHTML = 
+							"<h2>" + message + "</h2>";
+					}
+				);
+			}
+		);
+	}
+)

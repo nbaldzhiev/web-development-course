2c19834a3554ee956b61ab126b8821e760d003dc
nbaldzhiev <nenko.baldzhiev@gmail.com>
2021-01-04 00:12:49 +0200

    add lecture-60 files

---
+++ lecture-60/after/js/script.js
@@ -0,0 +1,35 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+(function (global) {
+	var dc = {};
+	var homeHtml = "snippets/home-snippet.html";
+
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+
+	document.addEventListener("DOMContentLoaded", function (event) {
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(homeHtml, function (responseText) {
+			insertHtml("#main-content", responseText);
+		}, false);
+	});
+
+	global.$dc = dc;
+})(window);
